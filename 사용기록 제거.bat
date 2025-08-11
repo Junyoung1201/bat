3717e49675f69afdb9341c4d7ed780efd82542de
준영 <leejunyoung5819@gmail.com>
2025-08-12 06:38:07 +0900

    크롬 사용기록 제거 업데이트

--- 사용기록 제거.bat
+++ 사용기록 제거.bat
@@ -1,9 +1,31 @@
+@echo off
+chcp 65001
 C:
 set chromeUserData=%LOCALAPPDATA%\Google\Chrome\User Data
+set chromeUserData2=%userprofile%\Local Settings\Application Data\Google\Chrome\User Data\Default
 del /s /q "%chromeUserData%\Default\History"
 del /s /q "%chromeUserData%\Default\History-journal"
 del /s /q "%chromeUserData%\Default\Sessions"
 rd /s /q "%chromeUserData%\Default\Cache"
+
+del /s /q "%chromeUserData2%\History"
+del /s /q "%chromeUserData2%\History-journal"
+del /s /q "%chromeUserData2%\Login Data"
+del /s /q "%chromeUserData2%\Login Data For Account"
+del /s /q "%chromeUserData2%\Login Data For Account-journal"
+del /s /q "%chromeUserData2%\Login Data-journal"
+del /s /q "%chromeUserData2%\Top Sites"
+del /s /q "%chromeUserData2%\Top Sites-journal"
+del /s /q "%chromeUserData2%\Visited Links"
+del /s /q "%chromeUserData2\Web Data"
+del /s /q "%chromeUserData2\Web Data-journal"
+rd /s /q "%chromeUserData2%\Accounts"
+rd /s /q "%chromeUserData2%\AutofillStrikeDatabase"
+rd /s /q "%chromeUserData2%\Cache"
+rd /s /q "%chromeUserData2%\IndexedDB"
+rd /s /q "%chromeUserData2%\Local Storage"
+rd /s /q "%chromeUserData2%\Session Storage"
+rd /s /q "%chromeUserData2%\Sessions"
 
 del /s /q "%appdata%\Adobe\Adobe Photoshop 2022\Adobe Photoshop 2022 Settings\MachinePrefs.psp"
 
