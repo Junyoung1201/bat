97eac780164ce5ce8d76e42dace90514cf52d062
준영 <leejunyoung5819@gmail.com>
2025-08-13 19:11:41 +0900

    업데이트

--- 사용기록 제거.bat
+++ 사용기록 제거.bat
@@ -1,44 +1,114 @@
 @echo off
-chcp 65001
+cls
+title 사용기록 제거 중
+
 C:
-set chromeUserData=%LOCALAPPDATA%\Google\Chrome\User Data
-set chromeUserData2=%userprofile%\Local Settings\Application Data\Google\Chrome\User Data\Default
-del /s /q "%chromeUserData%\Default\History"
-del /s /q "%chromeUserData%\Default\History-journal"
-del /s /q "%chromeUserData%\Default\Sessions"
-rd /s /q "%chromeUserData%\Default\Cache"
 
-del /s /q "%chromeUserData2%\History"
-del /s /q "%chromeUserData2%\History-journal"
-del /s /q "%chromeUserData2%\Login Data"
-del /s /q "%chromeUserData2%\Login Data For Account"
-del /s /q "%chromeUserData2%\Login Data For Account-journal"
-del /s /q "%chromeUserData2%\Login Data-journal"
-del /s /q "%chromeUserData2%\Top Sites"
-del /s /q "%chromeUserData2%\Top Sites-journal"
-del /s /q "%chromeUserData2%\Visited Links"
-del /s /q "%chromeUserData2\Web Data"
-del /s /q "%chromeUserData2\Web Data-journal"
-rd /s /q "%chromeUserData2%\Accounts"
-rd /s /q "%chromeUserData2%\AutofillStrikeDatabase"
-rd /s /q "%chromeUserData2%\Cache"
-rd /s /q "%chromeUserData2%\IndexedDB"
-rd /s /q "%chromeUserData2%\Local Storage"
-rd /s /q "%chromeUserData2%\Session Storage"
-rd /s /q "%chromeUserData2%\Sessions"
+set cDef_1=%LOCALAPPDATA%\Google\Chrome\User Data\Default
+set cDef_2=%userprofile%\Local Settings\Application Data\Google\Chrome\User Data\Default
 
-del /s /q "%appdata%\Adobe\Adobe Photoshop 2022\Adobe Photoshop 2022 Settings\MachinePrefs.psp"
+echo 크롬 사용기록 삭제
+del /s /q "%cDef_1%\History"
+del /s /q "%cDef_1%\History-journal"
+del /s /q "%cDef_1%\Login Data"
+del /s /q "%cDef_1%\Login Data For Account"
+del /s /q "%cDef_1%\Login Data For Account-journal"
+del /s /q "%cDef_1%\Login Data-journal"
+del /s /q "%cDef_1%\Top Sites"
+del /s /q "%cDef_1%\Top Sites-journal"
+del /s /q "%cDef_1%\Visited Links"
+del /s /q "%cDef_1%\Web Data"
+del /s /q "%cDef_1%\Web Data-journal"
+del /s /q "%cDef_1%\Account Web Data"
+del /s /q "%cDef_1%\Account Web Data-journal"
+del /s /q "%cDef_1%\Affiliation Database"
+del /s /q "%cDef_1%\Affiliation Database-journal"
+del /s /q "%cDef_1%\BrowsingTopicsSiteData"
+del /s /q "%cDef_1%\BrowsingTopicsSiteData-journal"
+del /s /q "%cDef_1%\BrowsingTopicsState"
+del /s /q "%cDef_1%\InterestGroups"
+del /s /q "%cDef_1%\SharedStorage"
+del /s /q "%cDef_1%\Shortcuts"
+del /s /q "%cDef_1%\Shortcuts-journal"
+del /s /q "%cDef_1%\Top Sites"
+del /s /q "%cDef_1%\Top Sites-journal"
+rd /s /q "%cDef_1%\Accounts"
+rd /s /q "%cDef_1%\AutofillStrikeDatabase"
+rd /s /q "%cDef_1%\Cache"
+rd /s /q "%cDef_1%\IndexedDB"
+rd /s /q "%cDef_1%\Local Storage"
+rd /s /q "%cDef_1%\Session Storage"
+rd /s /q "%cDef_1%\Sessions"
 
-del /s /q "%localappdata%\Packages\Microsoft.ZuneMusic_8wekyb3d8bbwe\LocalState\MediaPlayer.db-wal"
-del /s /q "%localappdata%\Packages\Microsoft.ZuneMusic_8wekyb3d8bbwe\LocalState\MediaPlayer.db-shm"
-del /s /q "%localappdata%\Packages\Microsoft.ZuneMusic_8wekyb3d8bbwe\LocalState\MediaPlayer.db"
+del /s /q "%cDef_2%\History"
+del /s /q "%cDef_2%\History-journal"
+del /s /q "%cDef_2%\Login Data"
+del /s /q "%cDef_2%\Login Data For Account"
+del /s /q "%cDef_2%\Login Data For Account-journal"
+del /s /q "%cDef_2%\Login Data-journal"
+del /s /q "%cDef_2%\Top Sites"
+del /s /q "%cDef_2%\Top Sites-journal"
+del /s /q "%cDef_2%\Visited Links"
+del /s /q "%cDef_2%\Web Data"
+del /s /q "%cDef_2%\Web Data-journal"
+del /s /q "%cDef_2%\Account Web Data"
+del /s /q "%cDef_2%\Account Web Data-journal"
+del /s /q "%cDef_2%\Affiliation Database"
+del /s /q "%cDef_2%\Affiliation Database-journal"
+del /s /q "%cDef_2%\BrowsingTopicsSiteData"
+del /s /q "%cDef_2%\BrowsingTopicsSiteData-journal"
+del /s /q "%cDef_2%\BrowsingTopicsState"
+del /s /q "%cDef_2%\InterestGroups"
+del /s /q "%cDef_2%\SharedStorage"
+del /s /q "%cDef_2%\Shortcuts"
+del /s /q "%cDef_2%\Shortcuts-journal"
+del /s /q "%cDef_2%\Top Sites"
+del /s /q "%cDef_2%\Top Sites-journal"
+rd /s /q "%cDef_2%\Accounts"
+rd /s /q "%cDef_2%\AutofillStrikeDatabase"
+rd /s /q "%cDef_2%\Cache"
+rd /s /q "%cDef_2%\IndexedDB"
+rd /s /q "%cDef_2%\Local Storage"
+rd /s /q "%cDef_2%\Session Storage"
+rd /s /q "%cDef_2%\Sessions"
 
-del /q /f /s "%APPDATA%\Microsoft\Windows\Recent\*"
-del /q /f /s "%TEMP%\*"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Login Data"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Login Data-journal"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Top Sites"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Top Sites-journal"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Visited Links"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Web Data"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Web Data-journal"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\GPUCache\*"
+del /s /q "%userprofile%\AppData\Local\User Data\Default\Sync Data\*"
+rd /s /q "%userprofile%\AppData\Local\User Data\Default\Thumbnails"
 
-del /q "C:\Windows\Memory.dmp" 2>nul
-del /q "C:\Windows\Minidump\*.dmp" 2>nul
+echo 기타 어플리케이션 캐시 파일 및 파일기록 데이터 삭제
+
+:: 토파츠 AI
+rd /q /s "%userprofile%\AppData\Local\mpv"
+rd /q /s "%localappdata%\Topaz Labs LLC\Topaz Photo AI\cache"
+
+:: 포토샵 2025 사진 열람 내역
+del /q /s "%appdata%\Adobe\Adobe Photoshop 2025\Adobe Photoshop 2025 Settings\MachinePrefs.psp"
+
+:: 마소 음악 앱
+del /q /s "%localappdata%\Packages\Microsoft.ZuneMusic_8wekyb3d8bbwe\LocalState\MediaPlayer.db-wal"
+del /q /s "%localappdata%\Packages\Microsoft.ZuneMusic_8wekyb3d8bbwe\LocalState\MediaPlayer.db-shm"
+del /q /s "%localappdata%\Packages\Microsoft.ZuneMusic_8wekyb3d8bbwe\LocalState\MediaPlayer.db"
+
+echo 임시 파일 삭제
+del /q /s "%APPDATA%\Microsoft\Windows\Recent\*"
+rd /q /s "%TEMP%\*"
+
+echo 윈도우 메모리 덤프 삭제
+del /s /q "C:\Windows\Memory.dmp"
+del /s /q "C:\Windows\Minidump\*.dmp"
+
+echo 윈도우 프리페쳐 삭제
+del /s /q "C:\Windows\Prefetch\*.*"
 
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
 
+:: 이벤트 로그 삭제
 wevtutil cl Application
